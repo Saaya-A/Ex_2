7fa459b26bcdc48d1223fbedbb7e7d099e1df4c6
saydi <saydi@ENGINEER>
2022-06-07 18:11:05 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите день: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("Введите месяц: ");
+        int b = int.Parse(Console.ReadLine());
+        Console.Write("Введите год: ");
+        int c = int.Parse(Console.ReadLine());
+        var calJul = new DateTime(c, b, a);
+        var calGri = calJul.AddDays((c / 100) - ((c / 100) / 4) - 2);
+        Console.WriteLine("Юлианский календарь - " + calJul.ToLongDateString());
+        Console.WriteLine("Современный календарь - " + calGri.ToLongDateString());
+    }
+}
